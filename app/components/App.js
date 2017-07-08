23a9c38c336a657dd2679dbc76db72de2cbdf34c
Todd <allento@sas.upenn.edu>
2017-07-08 17:46:37 -0400

    initialized

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import {Link, Route} from 'react-router-dom';
+// this will contain and render the very outer containers of each page of the website.
+// what is the best way to render things on a change of url, like when you want a completely different route and page?
+
+const App = () =>
+    <div>
+        This is working!
+    </div>;
+
+export default App;
